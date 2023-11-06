5c1691ad987f4a4fbd4a8ad48f44d470b172d84b
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-11-06 20:39:47 +0000

    fixing bug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,4 @@
 class MoviesController < ApplicationController
-  def index
-    matching_movies = Movie.all
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
-
-    render({ :template => "movie_templates/index" })
-  end
-
-  def show
-    the_id = params.fetch("path_id")
-
-    matching_movies = Movie.where({ :id => the_id })
-    @the_movie = matching_movies.at(0)
-
-    render({ :template => "movie_templates/show" })
-  end
-
   def create 
     m = Movie.new
     m.title = params.fetch("the_title")
@@ -39,4 +23,20 @@ class MoviesController < ApplicationController
 
     redirect_to("/movies")
   end 
+  
+  def index
+    matching_movies = Movie.all
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
+
+    render({ :template => "movie_templates/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_movies = Movie.where({ :id => the_id })
+    @the_movie = matching_movies.at(0)
+
+    render({ :template => "movie_templates/show" })
+  end
 end
